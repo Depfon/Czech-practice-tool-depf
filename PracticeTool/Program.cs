08242d5335035f43739d871956ea1dc08f550ba1
Artem Shashkov <128982406+Depfon@users.noreply.github.com>
2023-09-27 14:41:03 +0200

    feat(master): added program.cs

--- PracticeTool/Program.cs
+++ PracticeTool/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+        
+        
+    }
+}

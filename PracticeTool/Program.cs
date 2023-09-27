6840712240530c107ea5cc780a8ef506370f032a
Artem Shashkov <128982406+Depfon@users.noreply.github.com>
2023-09-27 15:34:54 +0200

    feat(master): added menu, basical features

--- PracticeTool/Program.cs
+++ PracticeTool/Program.cs
@@ -1,11 +1,14 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices;
+using MenuBuilder;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
         var startInfo = new ProcessStartInfo
         {
             FileName = "cmd.exe",
@@ -15,6 +18,20 @@ internal class Program
             CreateNoWindow = true
         };
         
+        Console.WriteLine("Выбирайте между элементами меню при помощи стрелок \u2195");
         
+        string[] MenuLines = { "- Режим тренировки", "- Бесконечный режим"};
+        
+        Menu BaseMenu = new Menu(MenuLines, 2, MenuLines.Length);
+        
+        BaseMenu.ModifyMenuCentered();
+        BaseMenu.CenterMenuToConsole();
+        BaseMenu.ResetCursorVisible();
+
+        switch (BaseMenu.RunMenu())
+        {
+            case 0:
+                new TrainingMode();
+        }
     }
 }
